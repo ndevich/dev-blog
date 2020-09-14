745139c03fca23e93e2ecc052c857bf1ea3eb4dd
Nadine Devich <nndevich@gmail.com>
2020-09-13 19:53:21 -0700

    Updates

---
+++ precache-manifest.9b8907057cff4514a596688441fef904.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbbd2ca2d9ad3e5be225a89c905913fe",
+    "url": "/dev-blog/index.html"
+  },
+  {
+    "revision": "f36a27a0574fb94fb3b9",
+    "url": "/dev-blog/static/css/main.2255c9c8.chunk.css"
+  },
+  {
+    "revision": "1cffdbe5866d0561008d",
+    "url": "/dev-blog/static/js/2.f852a86f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dev-blog/static/js/2.f852a86f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f36a27a0574fb94fb3b9",
+    "url": "/dev-blog/static/js/main.27d4f10f.chunk.js"
+  },
+  {
+    "revision": "7762601ee4e036d29cd7",
+    "url": "/dev-blog/static/js/runtime-main.7c9882e0.js"
+  },
+  {
+    "revision": "a6e5446a7c5789aabc9b37eaaf72134d",
+    "url": "/dev-blog/static/media/Oswald.a6e5446a.ttf"
+  },
+  {
+    "revision": "d0c75684f9b9b8826b2562246150b3af",
+    "url": "/dev-blog/static/media/lorem-ipsum.d0c75684.png"
+  },
+  {
+    "revision": "a604b167af95e98378b9e650eccd3dfc",
+    "url": "/dev-blog/static/media/office.a604b167.jpg"
+  }
+]);
--- src/components/footer/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../../app.css';
-
-const Footer = (): JSX.Element => <div></div>;
-
-export default Footer;
